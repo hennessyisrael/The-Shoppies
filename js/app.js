e1200101aeb5921dffab3ecebcfef0231d7a0e5f
Israel Akinola <gregsmedia2014@gmail.com>
2020-12-19 17:03:04 -0500

    12/19/2020 5:02

--- js/app.js
+++ js/app.js
@@ -19,108 +19,58 @@ const Nominations = [];
 function movie_to_html(){
     //Sort Movies In Desc Order
     movies.innerHTML = "";
-       if(MoviesData.length > 0){
-
-        // Latest Search
-        movies.innerHTML += `
-        <div class="movie current-movie">
-                    <img src="${ MoviesData[MoviesData.length - 1]['cover']}" alt="Movie Cover">
-                    <div class="content">
-                        <h5>${ MoviesData[MoviesData.length - 1]['title']}</h5>
-                        <p>Year of Release : ${ MoviesData[MoviesData.length - 1]['year']}</p>
-                        <p>Genre : ${ MoviesData[MoviesData.length - 1]['genre']}</p>
-                        <p>Director : ${ MoviesData[MoviesData.length - 1]['director']}</p>
-                        <button onclick="add_to_nomination(${ MoviesData[MoviesData.length - 1]['id']})">Add to Nomination</button>
-                    </div>
-            </div>
-        `
-    // Recently Search
-        for (i = 0; i < MoviesData.length -1 ; i++) {
-            
+        if(MoviesData && MoviesData.length > 0){
+    //  Search Result
+        for (i = 0; i < MoviesData.length; i++) {
             movies.innerHTML += `
             <div class="movie">
-                        <img src="${ MoviesData[i]['cover']}" alt="Movie Cover">
                         <div class="content">
-                            <h5>${ MoviesData[i]['title']}</h5>
-                            <p>Year of Release : ${ MovieData['year']}</p>
-                            <p>Genre : ${ MoviesData[i]['genre']}</p>
-                            <p>Director : ${ MoviesData[i]['director']}</p>
-                            <button onclick="add_to_nomination(${ MoviesData[i]['id']})">Add to Nomination</button>
+                            <h5>${ MoviesData[i].Title}</h5>
+                            <p>Year of Release : ${ MoviesData[i].Year}</p>
+                            <button class="nominate-btn" data-btn-title="${ MoviesData[i].Title}" onclick='add_to_nomination(${i}, "${MoviesData[i].Title}", event )'>Add to Nomination</button>
                         </div>
                 </div>
             `
         
-              } 
-       }
-}
-
-
-
-//Load the Movie Data to the Movies Array
-let movie_id = 0;
-function loadMovie(data){
-    if(data.Title){     
-        MovieData = {
-            id: movie_id,
-            cover : data.Poster,
-            title : data.Title,
-            genre : data.Genre,
-            director : data.Director,
-            year : data.Year,
-        }
-        MoviesData.push(MovieData);
-        movie_to_html();
-        movie_id++;
+        } 
+        disable_nominated_button();     
     }
-    else{
-        // Log User friendly Error to User if movie is undefined
-        error_ele.innerHTML = "Movie Title Doesn't Exist, Please try searching again with the correct title";
-    
-        
-    }
-   
 }
 
 
 
 //Get Movies
 function getMovie(moviename) {
-    return fetch(`http://omdbapi.com/?i=tt3896198&apikey=7497c18&t=${moviename}&type=movie`)
+    return fetch(`http://omdbapi.com/?i=tt3896198&apikey=7497c18&s=${moviename}&type=movie&page=9`)
         .then(
             function(response) {
                 if (response.status !== 200) {
                     console.log('Looks like there was a problem. Status Code: ' +
                         response.status);
                     return;
-                }
-
-                // Examine the text in the response
+                }                
                 response.json().then(function(data) {
-                     // IF Movie Title is aleady added to Search List
-                    for (i = 0; i < MoviesData.length; i++) {
-                        if(MoviesData[i].title == data.Title){
-                            alert("Movie Already Added");
-                            return
-                        }
-                    }
-
-                    loadMovie(data);
+                    MoviesData=data.Search;
+                    movie_to_html();
                 });
             }
         )
         //Log Error to console for dev analysis
         .catch(function(err) {
             console.log('Fetch Error :-S', err);
+            error_ele.innerHTML = "Movie Title Doesn't Exist, Please try searching again with the correct title";
         });
 }
 
 
 // Search and Display Movie Content
 function searchMovie(){
-    //    Alert user when Notification is Completed
-    if(Nominations.length == 5){
-        alert(" 5 Nomination Completed, Thanks!");
-        return
+    
+    // IF Movie Title is aleady added to Search List
+    for (i = 0; i < Nominations.length; i++) {
+        if(Nominations[i].title == MoviesData[i]['title']){
+                    
+            }
         }
 
 
@@ -130,86 +80,121 @@ function searchMovie(){
         movie_to_html()
        
     }
-       
-    
 }
-
 
 
 
 
 // Add Movie to Nominations 
-function add_to_nomination(id){
-    if(Nominations.length == 5){
-        alert(" 5 Nomination Completed, Thanks!");
-        return
+function add_to_nomination(movie_id,movie_title, e){
+   if(Nominations.length <= 4){
+        //Check if Movie not already Nominated
+    for (i = 0; i < Nominations.length; i++) {
+        if(Nominations[i].title === movie_title){
+            return
+        }
     }
-   
     // Push Obj To Nomination Array
-    Nominations.push(MoviesData[id]);
-     //Display Number of Nomination Length and load nomination to html
-    number_of_nominations.innerHTML = Nominations.length;
-    nomination_to_html()
-        
+   const Movie = {
+        id: movie_id,
+        title: movie_title,
+    }
 
+
+    Nominations.push(Movie);
+
+    nomination_to_html()
+    
+    
+    banner_nomination_check()
+   }
 }
+
+
+
+
+
+
+function disable_nominated_button(){
+    let btn = document.querySelectorAll('.nominate-btn');
+    // console.log(btn)
+    // for (i = 0; i < btn.length; i++){
+    //     for (j = 0; j < Nominations.length; j++){
+    //         if(btn[i].dataset.btnTitle == Nominations[j].title){
+    //             console.log("True")
+    //             // btn[i].disabled = true;
+    //             // btn[i].backgroundColor = "lightgrey";
+    //             // console.log(btn[i].disabled)
+    //             // console.log(Nominations[j].title)
+    //         }
+    //     }
+    // }
+}
+
+
+
+function delete_nominated_movie(id){
+    nominated.innerHTML = "";
+    //Remove Movie For Nominations array
+    for (i = 0; i < Nominations.length; i++) {
+        if(Nominations[i].id == id){
+            Nominations.splice(i, 1);  
+        }  
+    }
+
+        // Update Nomination Html
+        nomination_to_html();  
+
+        banner_nomination_check();
+}
+
+
 
 
 //Nominatiomn data to html
 function nomination_to_html(){
     nominated.innerHTML ="";
-    for (i = 0; i < Nominations.length; i++) {
-      nominated.innerHTML += `
-              <div class ="nominee">
-              <img src="${Nominations[i].cover}" alt="Movie Poster">
-              <span>${Nominations[i].title}</span>
-              <button class="delete" onclick="delete_nominated_movie(${Nominations[i].id})">X</button>
-              </div>
-          `
-  
-        } 
+    if(Nominations){
+        for (i = 0; i < Nominations.length; i++) {
+            nominated.innerHTML += `
+                    <div class ="nominee">
+                    <span>${Nominations[i].title}</span>
+                    <button class="delete" onclick="delete_nominated_movie(${Nominations[i].id})">X</button>
+                    </div>
+                `
+        
+              } 
+        //Update length of Nomination 
+        number_of_nominations.innerHTML = Nominations.length; 
+    }
+        
  }
 
-
-//Show Nominated Movies 
+  //Show Nominated Movies 
 function show_nominations(){
     if(Nominations.length > 0 ){
         nominated.classList.toggle("hide");
     }
 }
 
-
-
-function delete_nominated_movie(id){
-        nominated.innerHTML ="";
-        for (i = 0; i < Nominations.length; i++) {
-            if(Nominations[i].id == id){
-                Nominations.splice(i, 1);  
-            }  
+// Display a banner to the user when Nominatios Array length is up to 5
+function banner_nomination_check(){
+    banner = document.querySelector('.banner');
+       if(Nominations.length <= 4){
+            banner.classList.add('hide')
+        } 
+        else{
+            banner.classList.remove('hide')
         }
-
-        for (i = 0; i < Nominations.length; i++) {
-            nominated.innerHTML += `
-                <div class ="nominee">
-                <img src="${Nominations[i].cover}" alt="Movie Poster">
-                <span>${Nominations[i].title}</span>
-                <button class="delete" onclick="delete_nominated_movie(${Nominations[i].id})">X</button>
-                </div>
-                `
-        }
-           
-            number_of_nominations.innerHTML = Nominations.length;      
 }
 
-
-//Dark and Light Mode Toggle
-function changeMode(){
-    document.body.classList.toggle("dark");
-    header.classList.toggle("dark_sec");
-                                                
-    nominated.classList.toggle("dark_sec");
-
+function disable_nominated_button(){
+    let btn = document.querySelectorAll('.nominate-btn');
+    console.log(btn)
+    for (i = 0; i < btn.length; i++){
+        for (j = 0; j < Nominations.length; j++){
+           console.log(i);
+           console.log(j)
+        }
+    }
 }
-
-
-
